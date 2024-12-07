50fa34723ef9aa4139c4390a22a3d0b61c583599
zzhangblog <zzhangly@gmail.com>
2024-12-07 15:50:12 +0800

    Update Player.js

--- components/Player.js
+++ components/Player.js
@@ -72,10 +72,10 @@ const Player = () => {
           type='playlist'
           preload='auto'
           lrc-type={siteConfig('MUSIC_PLAYER_METING_LRC_TYPE')}
-          api={siteConfig(
-            'MUSIC_PLAYER_METING_API',
-            'https://api.i-meto.com/meting/api'
-          )}
+        //  api={siteConfig(
+        //    'MUSIC_PLAYER_METING_API',
+        //    'https://api.i-meto.com/meting/api'
+        //  )}
           autoplay={autoPlay}
           order={siteConfig('MUSIC_PLAYER_ORDER')}
           server={siteConfig('MUSIC_PLAYER_METING_SERVER')}
